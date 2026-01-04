3d30ea1150baae83125561f262e299d992d8144e
SimonCliffordNightscout <69906081+SimonCliffordNightscout@users.noreply.github.com>
2026-01-04 17:32:42 +0000

    Update theme.php

--- theme.php
+++ theme.php
@@ -15,8 +15,6 @@
         <link rel="icon" href="<?= $Wcms->asset('favicon.ico') ?>" type="image/x-icon">
         <link rel="shortcut icon" href="<?= $Wcms->asset('favicon.ico') ?>" type="image/x-icon">
 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
         <!-- Admin CSS -->
         <?= $Wcms->css() ?>
 
@@ -26,76 +24,54 @@
 
     <body>
     	<?= $Wcms->alerts() ?>
-
-    	<div class="settings-margin"><?= $Wcms->settings() ?></div>
+    	<?= $Wcms->settings() ?>
     	
-    	<!-- Navigation Bar -->
-    	<nav class="navbar navbar-default navbar-fixed-top">
-    		<div class="container">
-    			<div class="navbar-header">
-    				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu-collapse">
-    					<span class="icon-bar"></span>
-    					<span class="icon-bar"></span>
-    					<span class="icon-bar"></span>
-    				</button>
-    				<a class="navbar-brand" href="<?= $Wcms->url() ?>">
-    					<?= $Wcms->get('config', 'siteTitle') ?>
-    				</a>
-    			</div>
-    			<div class="collapse navbar-collapse" id="menu-collapse">
-    				<ul class="nav navbar-nav navbar-right">
-    					<?= $Wcms->menu() ?>
-    				</ul>
-    			</div>
+    	<!-- Top cream/yellow bar -->
+    	<div class="top-bar"></div>
+    	
+    	<!-- Banner section with green background -->
+    	<div class="banner-section">
+    		<div class="banner-container">
+    			<img src="<?= $Wcms->asset('Wrentham-Banner-8.jpg') ?>" alt="<?= $Wcms->get('config', 'siteTitle') ?>" class="banner-image">
+    		</div>
+    	</div>
+	
+    	<!-- Main navigation -->
+    	<nav class="main-nav">
+    		<div class="nav-container">
+    			<ul class="nav-menu">
+    				<?= $Wcms->menu() ?>
+    			</ul>
     		</div>
     	</nav>
-	
-    	<!-- Header with Page Title -->
-    	<header class="band-header">
-            <div class="container">
-                <h1 class="page-title"><?= $Wcms->page('title') ?></h1>
-            </div>
-    	</header>
 
         <!-- Main Content Area -->
-        <div class="container main-content">
-            <div class="row">
-                <!-- Main content column -->
-                <div class="col-md-8">
-                    <div class="content-card">
-                        <?= $Wcms->page('content') ?>
-                    </div>
+        <div class="content-wrapper">
+            <div class="content-container">
+                <!-- Main content column (left) -->
+                <div class="left-column">
+                    <?= $Wcms->page('content') ?>
                 </div>
                 
-                <!-- Sidebar -->
-                <div class="col-md-4">
-                    <div class="sidebar-section">
-                        <?= $Wcms->block('sidebar') ?>
-                    </div>
+                <!-- Sidebar (right) -->
+                <div class="right-column">
+                    <?= $Wcms->block('sidebar') ?>
                 </div>
             </div>
-        </div>
-
-        <!-- Footer Information Section -->
-        <div class="container-fluid footer-info">
-        	<div class="container">
-        		<div class="text-center">
-                    <?= $Wcms->block('footer-info') ?>
-        		</div>
-        	</div>
         </div>
 
         <!-- Footer -->
-        <footer class="container-fluid site-footer">
-        	<div class="container">
-                <div class="footer-content">
-                    <?= $Wcms->footer() ?>
-                </div>
+        <footer class="site-footer">
+        	<div class="footer-container">
+        		<ul class="footer-nav">
+        			<li><a href="<?= $Wcms->url() ?>">Home Page</a></li>
+        			<li><a href="<?= $Wcms->url('contact-us') ?>">Contact us</a></li>
+        			<li><a href="<?= $Wcms->url('engagements') ?>">Engagements</a></li>
+        		</ul>
+                <p class="footer-text"><?= $Wcms->footer() ?></p>
            </div>
         </footer>
 
-        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <?= $Wcms->js() ?>
 
     </body>
